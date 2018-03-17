73b64fc4cf2fff1259ffdc17a52fc253011acab5
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-03-17 09:58:25 -0300

    Vídeo 002

--- Startup.cs
+++ Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VideoEntityFrameworkCore.Data;
 
 namespace VideoEntityFrameworkCore
 {
@@ -21,6 +23,10 @@ namespace VideoEntityFrameworkCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlite(Configuration
+                    .GetConnectionString("DefaultConnection"))
+                );
             services.AddMvc();
         }
 
