72ac9480a9edf11ee73bbcc4786cc83dbe70eab8
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-04-02 20:33:06 -0300

    Vídeo 11

---
+++ wwwroot/images/js/methods_pt.js
@@ -0,0 +1,5 @@
+﻿jQuery.extend(jQuery.validator.methods, {
+    number: function (value, element) {
+        return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
+    }
+});
